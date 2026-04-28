0bfe9cba4f0186cfa19c041b0d88f04b715ae1fd
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-28 13:57:19 -0400

    Update and rename main.jsx to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.js'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-    <App/>
+      <App/>
     </Router>
   </React.StrictMode>,
 )
