2a5dddb0a5f1bfd8aeb2b65fb09281e162de9741
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-28 13:48:21 -0400

    Update exports and imports in index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from './home';
+import About from './about';
+import Designs from './designs';
+import BoatsAvailable from './boatsAvailable';
+import Contact from './contact';
+import Open350 from './models/Open350';
+import Open380 from './models/Open380';
+import Cat360 from './models/Cat360';
+import Tigress430 from './models/Tigress430';
+import Ultimate42 from './models/Ultimate42';
+import Passion50 from './models/Passion50';
+
+export { Home, About, Designs, BoatsAvailable, Contact, Open350, Open380, Cat360, Tigress430, Ultimate42, Passion50 };
--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import Apply from './applynow'
-import Home from './home'
-import TrafficControl from './trafficcontrol'
-
-
-export { Apply, Home, TrafficControl }
